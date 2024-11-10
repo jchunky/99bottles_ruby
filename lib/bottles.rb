922d795f09ebeab1db86e373e08485a29272263e
Jason CheongKeeYou <jasonc@nulogy.com>
2024-11-09 22:49:13 -0500

    refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,11 +8,27 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 3..99 then "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    when 2 then "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    when 1 then "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 0 then "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    end
+    "#{quantity(number)} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n".capitalize +
+      "#{action(number)}, #{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def action(number)
+    number == 0 ? "Go to the store and buy some more" : "Take #{pronoun(number)} down and pass it around"
+  end
+
+  def successor(number)
+    number == 0 ? 99 : number - 1
+  end
+
+  def quantity(number)
+    number == 0 ? "no more" : number.to_s
+  end
+
+  def container(number)
+    number == 1 ? "bottle" : "bottles"
+  end
+
+  def pronoun(number)
+    number == 1 ? "it" : "one"
   end
 end
