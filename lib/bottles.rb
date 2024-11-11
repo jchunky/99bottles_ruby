7dc1d79e565668cc5e13f13cd87600cf51998e10
Jason CheongKeeYou <jasonc@nulogy.com>
2024-11-11 02:08:38 -0500

    refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,30 +1,67 @@
 require "active_support/all"
 
 class BottleNumber < Struct.new(:number)
-  def self.for(number) = ("BottleNumber#{number}".safe_constantize || BottleNumber).new(number)
+  def self.for(number)
+    ("BottleNumber#{number}".safe_constantize || BottleNumber).new(number)
+  end
 
-  def action = "Take #{pronoun} down and pass it around"
-  def successor = BottleNumber.for(number - 1)
-  def quantity = number.to_s
-  def container = "bottles"
-  def pronoun = "one"
-  def to_s = "#{quantity} #{container}"
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def successor
+    BottleNumber.for(number - 1)
+  end
+
+  def quantity
+    number.to_s
+  end
+
+  def container
+    "bottles"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def to_s
+    "#{quantity} #{container}"
+  end
 end
 
 class BottleNumber0 < BottleNumber
-  def action = "Go to the store and buy some more"
-  def successor = BottleNumber.for(99)
-  def quantity = "no more"
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+
+  def quantity
+    "no more"
+  end
 end
 
 class BottleNumber1 < BottleNumber
-  def container = "bottle"
-  def pronoun = "it"
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
 end
 
 class Bottles
-  def song = verses(99, 0)
-  def verses(first, last) = first.downto(last).map { verse(_1) }.join("\n")
+  def song
+    verses(99, 0)
+  end
+
+  def verses(first, last)
+    first.downto(last).map { verse(_1) }.join("\n")
+  end
 
   def verse(number)
     number = BottleNumber.for(number)
